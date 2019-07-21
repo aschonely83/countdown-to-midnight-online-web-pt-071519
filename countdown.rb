de7e696306a82be7ab34c957882f8197176d6d82
Adam Schonely <aschonely@gmail.com>
2019-07-21 02:59:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,11 @@
-number = 10
+
+puts "HAPPY NEW YEAR!"
+ def countdown
+   number = 10
 while number > 0 
   puts "#{number} SECONDS"
   number -= 1 
 end
-puts "HAPPY NEW YEAR!"
- def countdown
  end
  
  def countdown_with_sleep(seconds_to_midnight)
