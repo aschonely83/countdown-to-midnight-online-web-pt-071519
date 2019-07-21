547af0531d7aede7d724963e2bb71b0eb7e03d7d
Adam Schonely <aschonely@gmail.com>
2019-07-21 02:51:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
-  number = 10
-  while number > 0 
-  puts "#{number} SECONDS"
-  number -= 1 
-  end
+number = 10
+while number > 0 
+puts "#{number} SECONDS"
+number -= 1 
+end
  def countdown
  end
